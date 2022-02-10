d527bd5eb0f23add063fea2432a96d12377dceea
kent <chauke@oregonstate.edu>
2022-02-10 09:16:49 -0800

    "test_clean_finalizing3"

--- smallsh.c
+++ smallsh.c
@@ -17,26 +17,39 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-//UPDATE
 
 //---------variables and functions
 void command_prompt();
-char *get_line();
 void expansion(char *source_string, char *find, char *exchange_with);
 void clean_up();
-//int execute_commands(char *first_command, char **arr_options );
 int execute_commands();
 void update_status(char *string);
 void add_pid(int x);
-// void kill_kids();
 int kill_kids();
-
 void enter_foreground(int x);
 void handler_interrupt(int z);
-void handler_twp(int w);
 int execute_commands_background();
 //-------------------------------
 
+/*
+ * Below is the imperative struct that will be referenced throughout
+ * this program.
+ *
+ * --members for command to hold the first command checked in
+ * by built in statuses or to the library of programs if not built in,
+ * --options list to hold arguments, or options for execution
+ * --less will hold redirect for input
+ * --greater holds redirect for greater than symbol
+ * --amper will hold a value for when there is an '&' in the user input for background execution
+ * --status holds status of previous child termination for calling by
+ * built-in function 'status'
+ * --pid array will hold child pid array
+ * for checking by kill kids function dictating which processes will be reaped
+ * --foreground member will hold value to indicate whether to initialize
+ * foreground mode by signal control Z
+ * --current holds value of the foreground child processes to dictated which children
+ * will be reaped and also what process to interrupt with control C
+ */
 struct Comms
 {
     char command[2048];
@@ -51,30 +64,22 @@ struct Comms
 
 } user_commands;
 
-//UPDATE
-void get_pids()
-{
-    int i;
-    // i = 0;
-    // for(i = 0; i < 4; i ++)
-    // {
-    //   printf("pid %d => %d\n", i, user_commands.pid_array[i]);
-    // }
-    // do
-    // {
-    //     if (user_commands.pid_array[i] != 0)
-    //     {
-    //         printf("pid #%d --> %d\n", i, user_commands.pid_array[i]);
-    //     }
-    //     i++;
-    // } while (user_commands.pid_array[i] != 0);
-}
+
+/*
+ * Will take in the status of an exited child and save it for
+ * calling from the built in 'status' command
+ */
 void update_status(char *string)
 {
     memset(user_commands.status, '\0', strlen(user_commands.status));
     strcpy(user_commands.status, string);
 }
-//ADDD PID
+
+/*
+ * Add pid function will add child pids to array in
+ * struct which will be checked on by the kill kids function
+ * returning termination status and reaping children
+ */
 void add_pid(int x)
 {
     int i;
@@ -89,11 +94,16 @@ void add_pid(int x)
 }
 
 /*
--------citation expansion function
+-------citation expansion function----
  date: 01/28/2022
  copied from /OR/ adapted from /OR/ based on:
  http://www.youtube.com/watch?v=yoa_mMmvlMc1
 -------
+ */
+/*
+ * Expansion function will take in the original string, character symbols,
+ * and the pid to exchange it with, will parse and rebuild the string in
+ * a character array, then copy it back to the original string for parsing
  */
 void expansion(char *source_string, char *find, char *exchange_with)
 {
@@ -113,23 +123,23 @@ void expansion(char *source_string, char *find, char *exchange_with)
         }
         memcpy(pointing_at, temp_string, p - temp_string);
         pointing_at += p -temp_string;
-
         memcpy(pointing_at, exchange_with, exchange_len);
         pointing_at +=exchange_len;
-
         temp_string = p +find_len;
     }
     strcpy(source_string, holder);
 }
+
+/*
+ * Function will be called to clean out struct for new user input
+ */
 void clean_up()
 {
     int i;
-
     memset(user_commands.command, '\0', 2048);
     memset(user_commands.less, '\0', 1000);
     memset(user_commands.greater, '\0', 1000);
     memset(user_commands.amper, '\0', 3);
-
 
     for (i = 0; i < sizeof (user_commands.options_list)/sizeof (user_commands.options_list[0]); i++)
     {
@@ -137,24 +147,9 @@ void clean_up()
         free(user_commands.options_list[i]);
         user_commands.options_list[i] = NULL;
     }
-//UPDATE
-    // i = 0;
-    // while(1)
-    // {
-    //     if (user_commands.options_list[i] == NULL)
-    //     {
-    //         break;
-    //     }
-    //     if(user_commands.options_list[i] != NULL){
-    //         free(user_commands.options_list[i]);
-    //         user_commands.options_list[i] = NULL;
-
-    //     }
-    //     i++;
-    // }
 }
 /*
-------citation for execute_commands
+------citation for execute_commands function below
  date: 02/01/2022
  copied from /OR/ adapted from /OR/ based on :
  https://www.geeksforgeeks.org/difference-fork-exec/
@@ -164,80 +159,57 @@ void clean_up()
  https://stackoverflow.com/questions/33508997/waitpid-wnohang-wuntraced-how-do-i-use-these/34845669
 ----------
  */
-
-//int execute_commands(char *first_command, char **arr_options)
+/*
+ * Function will be called to execute fork and exec
+ * will execute as a foreground child due to user input
+ * takes in arguments and creates a new array specifically
+ * for exec
+ */
 int execute_commands()
 {
     char *args[512] = {NULL};
-    int num_elements;
     int index_args;
     int index_arr_options;
-    int i;
     index_arr_options = 0;
-    num_elements = 0;
     index_args = 1;
-//    args[0] = first_command;
-    pid_t first_pid;
-    pid_t second_pid;
-    int state;
+
     args[0] = user_commands.command;
     while(1)
     {
-//        if (arr_options[index_arr_options] == NULL)
         if(user_commands.options_list[index_arr_options] == NULL)
         {
             break;
         }
-//        args[index_args] = arr_options[index_arr_options];
         args[index_args] = user_commands.options_list[index_arr_options];
         index_arr_options++;
         index_args++;
     }
-    //---------handle redirections
-//    if ((strcmp(user_commands.greater, "") != 0))//not empty
-//    {
-//        int targetFD = open(user_commands.greater, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-//        if (targetFD == -1)
-//        {
-//            perror("target open()");
-//            exit(1);
-//        }
-//        dup2(targetFD, 1);
-//        fcntl(targetFD, F_SETFD, FD_CLOEXEC);a
-//    }
-    //-----------
 
     pid_t spawnPid;
     int childExitStatus;
     user_commands.pid_array[0] = 1;
     spawnPid = fork();
-    // int pid = (waitpid(-1, NULL, WNOHANG));
-    // printf("pid result --> %d; the pide is --> %d\n", pid, spawnPid);
     switch(spawnPid)
     {
-        // if (spawnPid == -1)
         case -1:
         {
             perror("fork failed!");
             fflush(stdout);
             clean_up();
             break;
-            // exit(1);
         }
-            // else if (spawnPid == 0)
         case 0:
         {
-            // printf("child (%d) \n", getpid());
-            // fflush(stdout);
-            // usleep(5);
-            // write(0, "\n", 1);
-//        sleep(10);
+
+            /*
+            * Redirect input depending on if a '>' was found
+            * in user input and parsed out
+            */
             if ((strcmp(user_commands.greater, "") != 0))//not empty
             {
                 int targetFD = open(user_commands.greater, O_WRONLY | O_CREAT | O_TRUNC, 0777);
                 if (targetFD == -1)
                 {
-//                perror("target open()");
                     printf("ERROR. cannot open");
                     fflush(stdout);
                     clean_up();
@@ -248,13 +220,15 @@ int execute_commands()
                 fflush(stdout);
 
             }
-
+            /*
+             * Redirect input depending on if a '<' was found
+             * in user input and parsed out
+             */
             if ((strcmp(user_commands.less, "") != 0))//not empty
             {
                 int source_file = open(user_commands.less, O_RDONLY);
                 if (source_file == -1)
                 {
-//                perror("cannot open for input");
                     printf("error cannot open %s for input\n", user_commands.less);
                     fflush(stdout);
                     clean_up();
@@ -263,42 +237,24 @@ int execute_commands()
                 dup2(source_file, 0);
                 fcntl(source_file, FD_CLOEXEC);
             }
-//EXECUTION
             execvp(user_commands.command, args);
-            // perror("Foreground: exec failed\n");
 
             perror(user_commands.command);
             clean_up();
             exit(2);
-            break;
-            // return 2;
         }
-            // else
         default:
         {
-            // printf("FOREGROUND childs pid = %d\n", spawnPid);
-            // write(0, "\n", 1);
-            //-----adding pids to array
 
-            //--------------
             fflush(stdout);
-
-//        spawnPid = waitpid(spawnPid, &childExitStatus, WNOHANG);
-
             user_commands.current = spawnPid;
-            // printf("FOREGROUND \n");
-            // fflush(stdout);
 
             spawnPid = (waitpid(spawnPid, &childExitStatus, 0));
 
             if (WIFEXITED(childExitStatus))
             {
-                // printf ("current FOREGROUND PID --> %d\n", user_commands.current);
-                // printf("FOREGROUND exit status child--> %d\n", WIFEXITED(childExitStatus));
-                // printf("FOREGROUND child %d exited normal with %d\n", spawnPid, WEXITSTATUS(childExitStatus));
                 char normal1[70] = {'\0'};
                 sprintf(normal1, "exit status %d", WEXITSTATUS(childExitStatus));
-                // update_status("exit status 0");
                 update_status(normal1);
                 clean_up();
                 fflush(stdout);
@@ -307,8 +263,6 @@ int execute_commands()
             }
             else
             {
-                // printf("FOREGROUND exit status child--> %d\n", WIFEXITED(childExitStatus));
-                // printf("FOREGROUND child %d ABNORMALLY exited due to signal %d\n", spawnPid, WTERMSIG(childExitStatus));
                 char abnormal[70] = {'\0'};
                 sprintf(abnormal, "terminated by signal %d", WTERMSIG(childExitStatus));
                 update_status(abnormal);
@@ -317,19 +271,17 @@ int execute_commands()
                 break;
 
             }
-
-            user_commands.pid_array[0] = 0;
-            clean_up();
-            exit(0);
-
         }
-            // exit(0);
     }
-    // exit();
     return 0;
 
 }
-
+/*
+ * Function will be called to execute fork and exec
+ * will execute as a background child due to user input
+ * takes in arguments and creates a new array specifically
+ * for exec
+ */
 int execute_commands_background()
 {
     char *args[512] = {NULL};
@@ -468,7 +420,7 @@ int kill_kids()
     int result;
     int status;
     char stat[1000] = {0};
-    get_pids();
+
 
 
     for (i = 1; i < 512; i++)
@@ -699,8 +651,7 @@ void command_prompt()
                 change_result = chdir(user_commands.options_list[0]);
                 if (change_result == 0)
                 {
-                    printf("dir changed\n");
-                    fflush(stdout);
+
                     if (getcwd(buff, sizeof buff) == NULL)
                     {
                         perror("cwd error\n");
